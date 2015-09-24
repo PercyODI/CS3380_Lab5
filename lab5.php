39188f9c416db4d343dcfdca9bbcf5aca77664dc
PercyODI <pearse.2008@gmail.com>
2015-09-24 21:27:28 +0000

    Fixed SQL Statements layout and added Query Descriptions

--- lab5.php
+++ lab5.php
@@ -9,7 +9,7 @@
       }
       
       body {
-        padding-top: 70px;
+        padding-top: 50px;
         padding-bottom: 70px;
       }
       .nav-bottom-cust {
@@ -18,6 +18,18 @@
       
       .nav-top-cust {
         border-bottom: 1px solid lightslategray;
+      }
+      
+      .table-cust {
+        max-width: 50%;
+        margin: auto;
+      }
+      
+      .well {
+        max-width: 51%;
+        margin: auto;
+        margin-bottom: 10px;
+        font-size: 16px;
       }
       
       /*.fa-globe {*/
@@ -45,87 +57,120 @@
       $mylink = mysqli_connect( $SERVER, $USER, $PASS, $DATABASE) or die("<h3>Sorry, could not connect to database.</h3><br/>Please contact your system's admin for more help\n");
       $_SESSION['mylink'] = $mylink;
       
-      $query_list = array(    
-                  "SELECT District, Population 
-                    FROM city 
-                    WHERE Name='Springfield' 
-                    ORDER BY Population DESC",
-                  "SELECT name, district, population 
-                    FROM city 
-                    WHERE CountryCode = 'BRA' 
-                    ORDER BY name",
-                  "SELECT name, continent, surfacearea AS `Surface Area` 
-                    FROM country 
-                    ORDER BY surfacearea 
-                    LIMIT 20",
-                  "SELECT name, 
-                          continent, 
-                          governmentform AS `Form of Government`, 
-                          gnp AS `GNP` 
-                    FROM country 
-                    WHERE gnp > 200000 
-                    ORDER BY name",
-                  "SELECT name 
-                    FROM country 
-                    WHERE lifeexpectancy IS NOT NULL 
-                    ORDER BY lifeexpectancy 
-                    LIMIT 10 
-                    OFFSET 9",
-                  "SELECT name 
-                    FROM city 
-                    WHERE name like 'B%s' 
-                    ORDER BY population DESC",
-                  "SELECT city.name AS `City Name`, 
-                          country.name AS `Country Name`, 
-                          city.population AS `City Population` 
-                    FROM city 
-                    INNER JOIN country 
-                    ON city.countrycode = country.code 
-                    WHERE city.population > 6000000 
-                    ORDER BY city.population DESC",
-                  "SELECT country.name AS `Country Name`, 
-                          country.indepyear AS `Year of Independence`, 
-                          country.region 
-                    FROM country 
-                    INNER JOIN countrylanguage 
-                    ON country.code = countrylanguage.countrycode 
-                    WHERE countrylanguage.language = 'English' 
-                    AND countrylanguage.isofficial='T' 
-                    ORDER BY country.region, country.name",
-                  "SELECT countryName AS `Country Name`, 
-                          cityName AS `City Name`, 
-                          (cityPop / countryPop) AS `Percent of Population In Capital` 
-                    FROM 
-                      (SELECT country.capital, country.name AS 'countryName', 
-                              city.name AS 'cityName', 
-                              city.population AS 'cityPop', 
-                              country.population AS 'countryPop' 
-                      FROM city 
-                      INNER JOIN country 
-                      ON country.capital = city.id) AS table1 
-                    ORDER BY `Percent of Population In Capital` DESC",
-                  "SELECT language, 
-                          name, 
-                          ((percentage * population) / 100) AS `Percentage of Speakers` 
-                    FROM country 
-                    INNER JOIN 
-                      (SELECT countrycode, language, percentage 
-                      FROM countrylanguage 
-                      WHERE isOfficial = 'T') AS languageTable 
-                    ON country.code = languageTable.countrycode 
-                    ORDER BY `Percentage of Speakers` DESC",
-                  "SELECT name, 
-                          region, 
-                          gnp AS `GNP`, 
-                          gnpold AS `Old GNP`, 
-                          ((gnp - gnpold) / gnpold) AS `Real GNP Change` 
-                    FROM country 
-                    WHERE gnp IS NOT NULL 
-                    AND gnpold IS NOT NULL 
-                    ORDER BY `Real GNP Change` DESC");
-                  // 10 11
-                  // Cross off when completed.
-                  // Come back to 5...seems off.
+      $query_list = array(
+        // Query 1  
+        "SELECT District, Population 
+          FROM city 
+          WHERE Name='Springfield' 
+          ORDER BY Population DESC",
+        // Query 2
+        "SELECT name, district, population 
+          FROM city 
+          WHERE CountryCode = 'BRA' 
+          ORDER BY name",
+        // Query 3
+        "SELECT name, continent, surfacearea AS `Surface Area` 
+          FROM country 
+          ORDER BY surfacearea 
+          LIMIT 20",
+        // Query 4
+        "SELECT name, 
+                continent, 
+                governmentform AS `Form of Government`, 
+                gnp AS `GNP` 
+          FROM country 
+          WHERE gnp > 200000 
+          ORDER BY name",
+        // Query 5
+        "SELECT name 
+          FROM country 
+          WHERE lifeexpectancy IS NOT NULL 
+          ORDER BY lifeexpectancy 
+          LIMIT 10 
+          OFFSET 9",
+        // Query 6
+        "SELECT name 
+          FROM city 
+          WHERE name like 'B%s' 
+          ORDER BY population DESC",
+        // Query 7
+        "SELECT city.name AS `City Name`, 
+                country.name AS `Country Name`, 
+                city.population AS `City Population` 
+          FROM city 
+          INNER JOIN country 
+          ON city.countrycode = country.code 
+          WHERE city.population > 6000000 
+          ORDER BY city.population DESC",
+        // Query 8
+        "SELECT country.name AS `Country Name`, 
+                country.indepyear AS `Year of Independence`, 
+                country.region 
+          FROM country 
+          INNER JOIN countrylanguage 
+          ON country.code = countrylanguage.countrycode 
+          WHERE countrylanguage.language = 'English' 
+          AND countrylanguage.isofficial='T' 
+          ORDER BY country.region, country.name",
+        // Query 9
+        "SELECT countryName AS `Country Name`, 
+                cityName AS `City Name`, 
+                (cityPop / countryPop) AS `Percent of Population In Capital` 
+          FROM 
+            (SELECT country.capital, country.name AS 'countryName', 
+                    city.name AS 'cityName', 
+                    city.population AS 'cityPop', 
+                    country.population AS 'countryPop' 
+            FROM city 
+            INNER JOIN country 
+            ON country.capital = city.id) AS table1 
+          ORDER BY `Percent of Population In Capital` DESC",
+        // Query 10
+        "SELECT language, 
+                name, 
+                ((percentage * population) / 100) AS `Percentage of Speakers` 
+          FROM country 
+          INNER JOIN 
+            (SELECT countrycode, language, percentage 
+            FROM countrylanguage 
+            WHERE isOfficial = 'T') AS languageTable 
+          ON country.code = languageTable.countrycode 
+          ORDER BY `Percentage of Speakers` DESC",
+        // Query 11
+        "SELECT name, 
+                region, 
+                gnp AS `GNP`, 
+                gnpold AS `Old GNP`, 
+                ((gnp - gnpold) / gnpold) AS `Real GNP Change` 
+          FROM country 
+          WHERE gnp IS NOT NULL 
+          AND gnpold IS NOT NULL 
+          ORDER BY `Real GNP Change` DESC");
+    
+    $query_descriptions = array (
+      // Query 1
+      "The district and population of all cities named Springfiled",
+      // Query 2
+      "The name, district, and population of each city in Brazil",
+      // Query 3
+      "The 20 smallest countries by surface area",
+      // Query 4
+      "All countries with a GNP greater than 200,000",
+      // Query 5
+      "The 10th through the 19th countries in life expectancy",
+      // Query 6
+      "Al cities that start with the letter 'B' and end in the letter 's'",
+      // Query 7
+      "All cities with a population greater than 6 million",
+      // Query 8
+      "All countries where English is an offical language, and their year of independence",
+      // Query 9
+      "Capital cities and the percentage of the county population in the captial",
+      // Query 10
+      "Offical language of all cities and the number of speakers of that language",
+      // Query 11
+      "The GNP for all countries, sorted by the most improved relative wealth"
+      );  
       
       function run_sql_query() {
         global $query_list;
@@ -134,16 +179,16 @@
         }
         $result = mysqli_query($_SESSION['mylink'], $query_list[$_POST['sqlDropDown']]);
         
-        $columns = mysqli_fetch_fields($result);
+        $column_names = mysqli_fetch_fields($result);
         
         $j = 0;
         while($data = mysqli_fetch_row($result)) {
-          for($i = 0; $i < count($columns); $i++) {
-            $return[$j][] = $data[$i - 0];
+          for($i = 0; $i < count($column_names); $i++) {
+            $query_data[$j][] = $data[$i - 0];
           }
           $j++;
         }
-        return array($columns, $return);
+        return array($column_names, $query_data);
       }
     ?>
     <meta name="viewport" content="width=device-width, initial-scale=1">
@@ -176,22 +221,24 @@
         
         <?php
           if(isset($_POST['sqlDropDown'])) {
-            echo "<table class='table table-hover table-striped'>";
+            echo "<div class='well text-center'><b>" . $query_descriptions[$_POST['sqlDropDown']] . "</b></div>";
+            
+            echo "<table class='table table-hover table-striped table-cust'>";
             // Runs the sql query, the query is stored in return, 
             // The column names are stored in columns
-            list($columns, $return) = run_sql_query($query_list[3]);
+            list($column_names, $query_data) = run_sql_query($query_list[3]);
             
-            // Creates the table headers based off of $columns
+            // Creates the table headers based off of $column_names
             echo "<tr>";
-            foreach($columns as $value) {
+            foreach($column_names as $value) {
               echo "<th class='text-center'>" . ucwords($value->name) . "</th>";
             }
             echo "</tr>";
             
             //Creates the table data
-            foreach($return as $value) {
+            foreach($query_data as $value) {
               echo "<tr>\n";
-              for($i = 0; $i < count($columns); $i++) {
+              for($i = 0; $i < count($column_names); $i++) {
                 is_numeric($value[$i]) ? $textAlign = " class='text-right'" : $textAlign = "";
                 echo "<td$textAlign>" . $value[$i] . "</td>";
               }
@@ -212,7 +259,7 @@
         <button class='btn btn-primary navbar-right ' id='queryResults'>
           <?php
           if(isset($_POST['sqlDropDown'])) {
-            echo "Number of query results: <span class='badge'>" . count($return) . "</span>";
+            echo "Number of query results: <span class='badge'>" . count($query_data) . "</span>";
           } else {
             echo "No Query Selected";
           }
